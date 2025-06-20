a24d4288cfc92b98813ecfddf9b55f83df98b7a9
Aya Sulyman <ayasulyiman@gmail.com>
2025-06-20 15:20:31 +0300

    added

--- db/mongoose.js
+++ db/mongoose.js
@@ -1,10 +1,13 @@
-const mongoose = require('mongoose');
+   const mongoose = require('mongoose');
+   require('dotenv').config(); 
+   const uri = process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/de-vision'; 
 
-mongoose.connect('mongodb://127.0.0.1:27017/de-vision', {
-    useNewUrlParser: true,
-    useUnifiedTopology: true
-}).then(() => {
-    console.log("Connected to MongoDB");
-}).catch((error) => {
-    console.error("MongoDB connection error:", error);
-});
+   mongoose.connect(uri, {
+       useNewUrlParser: true,
+       useUnifiedTopology: true
+   }).then(() => {
+       console.log("Connected to MongoDB");
+   }).catch((error) => {
+       console.error("MongoDB connection error:", error);
+   });
+   
